75a851761d4fa55411055cae64861f90a22cc185
mancjames <multicolouredsmurf@gmail.com>
2022-07-22 20:38:18 +0100

    added Layout component and added to app.js

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import NavBar from './Navbar';
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+        <Head>
+            <title>Ecommerce Store</title>
+        </Head>
+        <header>
+            <NavBar />
+        </header>
+        <main className="main-container">
+            {children}
+        </main>
+        <footer>
+            <Footer />
+        </footer>
+    </div>
   )
 }
 
