e2fe61ad25a5d0ec1f6e90d5ab33378fd3e20356
mancjames <multicolouredsmurf@gmail.com>
2022-07-25 16:56:24 +0100

    added state management using useContext hook with React - added functionality to allow adding items to cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import { client, urlFor } from '../../lib/client'
 import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai'
 import { Product } from '../../components'
+import { useStateContext } from '../../context/StateContext'
 
 const ProductDetails = ({product, products}) => {
-    const {image, name, details, price} = product
+    const {image, name, details, price} = product;
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd} = useStateContext()
   return (
     <div>
         <div className="product-detail-container">
@@ -27,7 +29,7 @@ const ProductDetails = ({product, products}) => {
             </div>
 
             <div className="product-detail-desc">
-                <h1>{name}</h1>
+                <h1>{name}</h1> 
                 <div className="reviews">
                     <div>
                         <AiFillStar />
@@ -46,13 +48,13 @@ const ProductDetails = ({product, products}) => {
                 <div className="quantity">
                     <h3>Quantity:</h3>
                     <p className="quantity-desc">
-                        <span className="minus" onClick=""><AiOutlineMinus /></span>
-                        <span className="num" onClick="">0</span>
-                        <span className="plus" onClick=""><AiOutlinePlus /></span>
+                        <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+                        <span className="num" onClick="">{qty}</span>
+                        <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
                     </p>
                 </div>
                 <div className="buttons">
-                    <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
+                    <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
                     <button type="button" className="buy-now" onClick="">Buy Now</button>
                 </div>
             </div>
